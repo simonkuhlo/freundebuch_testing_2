789185e19b95bb42c1f7bcdcd263b193ece74f5e
simonkuhlo <siga241@gmail.com>
2024-05-23 08:02:45 +0200

    db

---
+++ TemplateData/forms.py
@@ -0,0 +1,19 @@
+from TemplateData import models as td
+from UserData import models as ud
+from django import forms
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = ud.Answer
+        fields = ['text']  # Assuming 'text' is the field for the answer in your Answer model
+
+    def __init__(self, *args, **kwargs):
+        self.question = kwargs.pop('question', None)
+        super(QuestionForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super(QuestionForm, self).save(commit=False)
+        instance.question = self.question
+        if commit:
+            instance.save()
+        return instance
--- freundebuch_testing_2/urls.py
+++ freundebuch_testing_2/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('<str:lang>/view/', include('TemplateData.urls'), name = 'view'),
+    path('<str:lang>/view/', include('UserData.urls'), name = 'view'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
